0b18886baefe235a4d2efec8e680b5d70be66148
ricky.peng <ricky.peng@corp.to8to.com>
2021-03-11 09:52:46 +0800

    【修改】- 删除测试代码

--- web_app/android/app/src/main/kotlin/com/ricky/web_app/LaunchActivity.kt
+++ web_app/android/app/src/main/kotlin/com/ricky/web_app/LaunchActivity.kt
@@ -7,12 +7,11 @@ class LaunchActivity: Activity() {
     override fun onStart() {
         super.onStart()
         intent= Intent(this, MainActivity::class.java)
-        Intent.FLAG_ACTIVITY_NEW_DOCUMENT
         startActivity(intent)
     }
 
     override fun onStop() {
         super.onStop()
-        finish()
+//        finish()
     }
 }
